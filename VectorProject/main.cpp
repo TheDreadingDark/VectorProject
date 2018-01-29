843297f49ff637973045cc1f660608ae8e3ac2d7
TheDreadingDark <31518923+TheDreadingDark@users.noreply.github.com>
2018-01-29 12:44:45 -0700

    Initial Commit

---
+++ VectorProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VectorProject
+//
+//  Created by Woolsey, Adam on 1/29/18.
+//  Copyright © 2018 Woolsey, Adam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
